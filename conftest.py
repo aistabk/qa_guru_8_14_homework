8ffa700cbaf9469e2b44350807c0af39e6720f2e
aistabk <elizavetasoom@ya.ru>
2023-11-02 15:02:59 +0300

    conftest changes

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
+import os
+
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
-
+from dotenv import load_dotenv
 from utils import attach
 
 
@@ -18,9 +20,13 @@ def setup_browser(request):
             "enableVideo": True
         }
     }
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
